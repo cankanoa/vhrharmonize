c9d8ff92755ed46f4eb6e7e8544809783dacadf9
cankanoa <cankanoa@gmail.com>
2025-04-30 17:33:34 -1000

    Enhance debug output and fix bugs
    Corrected case sensitivity issues in input folder paths to ensure compatibility across different operating systems. Added optional debug mode messages to display the number of parallel workers used. Adjusted import statements to use relative imports for better package organization. Fix bugs.

--- vhrharmonize/__init__.py
+++ vhrharmonize/__init__.py
@@ -1,8 +1,8 @@
-from find_files import find_files, find_roots, get_metadata_from_files
-from utils import shp_to_gpkg, get_image_largest_value, wsl_to_windows_path
-from rpc_orthorectification import qgis_gcps_to_geojson, gcp_refined_rpc_orthorectification
-from pansharpen import pansharpen_image
-from flaash import run_flaash
+from .find_files import find_files, find_roots, get_metadata_from_files
+from .utils import shp_to_gpkg, get_image_largest_value, wsl_to_windows_path
+from .rpc_orthorectification import qgis_gcps_to_geojson, gcp_refined_rpc_orthorectification
+from .pansharpen import pansharpen_image
+from .flaash import run_flaash
 
 __all__ = [
     'find_files',
